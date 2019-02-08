f7ba6ab35f94d5f6cb82c2209c4c392a0e8ef255
Payten Giles <payten.giles@gmail.com>
2019-02-08 12:08:58 +1100

    Allow relationships to filter based on a jsonmodel_type when present.  This allows multiple relationships to use the same relationship table on the one record type, whereby each relationship defines which relationship jsonmodels are supported.
    (cherry picked from commit 88a89821cbaf543af4ed5296663473509f31e28d)

--- backend/app/model/mixins/relationships.rb
+++ backend/app/model/mixins/relationships.rb
@@ -318,6 +318,8 @@ AbstractRelationship = Class.new(Sequel::Model) do
 
   def self.wants_array?; @wants_array; end
 
+  def self.set_supported_jsonmodel_types(supported_jsonmodel_types); @supported_jsonmodel_types = supported_jsonmodel_types; end
+  def self.supported_jsonmodel_types; @supported_jsonmodel_types; end
 
   # Return a list of the relationship instances that refer to 'obj'.
   def self.find_by_participant(obj)
@@ -737,6 +739,7 @@ module Relationships
           set_participating_models([base, *related_models].uniq)
           set_json_property(opts[:json_property])
           set_wants_array(opts[:is_array].nil? || opts[:is_array])
+          set_supported_jsonmodel_types(opts[:supported_jsonmodel_types])
         end
 
         opts[:class_callback].call(clz) if opts[:class_callback]
@@ -891,6 +894,9 @@ module Relationships
 
           json[property_name] = relationships.map {|relationship|
             next if RequestContext.get(:enforce_suppression) && relationship.suppressed == 1
+            if relationship_defn.supported_jsonmodel_types && relationship.respond_to?(:jsonmodel_type)
+              next unless relationship_defn.supported_jsonmodel_types.include?(relationship.jsonmodel_type)
+            end
 
             # Return the relationship properties, plus the URI reference of the
             # related object
@@ -898,7 +904,7 @@ module Relationships
             values['ref'] = relationship.uri_for_other_referent_than(obj)
 
             values
-          }
+          }.compact
 
           if !relationship_defn.wants_array?
             json[property_name] = json[property_name].first
