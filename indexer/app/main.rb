fcb7288b440ce5bf7fcd2b52bdfd7b3c817a1f99
Mark Triggs <mark@dishevelled.net>
2019-05-01 07:09:45 +1000

    Move up plugin loading so plugin indexers are loaded
    (cherry picked from commit 374c3341ba8cb2ff06ee6fa1c713d4f04e352f2c)

--- indexer/app/main.rb
+++ indexer/app/main.rb
@@ -53,6 +53,20 @@ class ArchivesSpaceIndexer < Sinatra::Base
       end
     end
 
+    configure do
+      begin
+        # Load plugin init.rb files (if present)
+        ASUtils.order_plugins(ASUtils.find_local_directories('indexer')).each do |dir|
+          init_file = File.join(dir, "plugin_init.rb")
+          if File.exists?(init_file)
+            load init_file
+          end
+        end
+      rescue
+        ASUtils.dump_diagnostics($!)
+      end
+    end
+
     AppConfig[:plugin_indexers].each_with_index do |indexer_config, ix|
       indexer = indexer_config[:class].constantize.get_plugin_indexer(indexer_config)
 
@@ -112,20 +126,7 @@ class ArchivesSpaceIndexer < Sinatra::Base
     threads.each {|t| t.join} if java.lang.System.get_property("aspace.devserver")
   end
 
-
   configure do
-    begin
-      # Load plugin init.rb files (if present)
-      ASUtils.order_plugins(ASUtils.find_local_directories('indexer')).each do |dir|
-        init_file = File.join(dir, "plugin_init.rb")
-        if File.exists?(init_file)
-          load init_file
-        end
-      end
-    rescue
-      ASUtils.dump_diagnostics($!)
-    end
-
     set :logging, false 
     Log.noisiness "Logger::#{AppConfig[:indexer_log_level].upcase}".constantize
 
