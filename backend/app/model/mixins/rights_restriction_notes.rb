5c158d91b7bf11835033299f8530f65e4ba1d4fd
Mark Triggs <mark@dishevelled.net>
2019-10-15 09:28:10 +1100

    QSA migration performance: Skip rights restriction notes
    (cherry picked from commit e279bb942dc46580bd07aece9f235a823736be45)

--- backend/app/model/mixins/rights_restriction_notes.rb
+++ backend/app/model/mixins/rights_restriction_notes.rb
@@ -12,7 +12,7 @@ module RightsRestrictionNotes
 
   def update_from_json(json, opts = {}, apply_nested_records = true)
     obj = super
-    RightsRestrictionNotes::Implementation.process_restriction_notes(json, obj)
+    # RightsRestrictionNotes::Implementation.process_restriction_notes(json, obj)
     obj
   end
 
@@ -21,7 +21,7 @@ module RightsRestrictionNotes
 
     def create_from_json(json, opts = {})
       obj = super
-      RightsRestrictionNotes::Implementation.process_restriction_notes(json, obj)
+      # RightsRestrictionNotes::Implementation.process_restriction_notes(json, obj)
       obj
     end
 
