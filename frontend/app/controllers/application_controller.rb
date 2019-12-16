4bf08889378eb2aea335a768d141daa75dfd1af7
Mark Triggs <mark@dishevelled.net>
2019-12-17 10:40:26 +1100

    Rename field to make sure existing JS works when calculating available boolean ops
    (cherry picked from commit 392923e762225e6269b5e4091a1d2f0d523f0549)

--- frontend/app/controllers/application_controller.rb
+++ frontend/app/controllers/application_controller.rb
@@ -768,8 +768,8 @@ class ApplicationController < ActionController::Base
       end
 
       if query["type"] == "range"
-        query["from"] = params["fv#{i}"]
-        query["to"] = params["tv#{i}"]
+        query["from"] = params["vf#{i}"]
+        query["to"] = params["vt#{i}"]
         query.delete('value')
       end
 
