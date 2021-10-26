3739c2b080e76ee849a678d1305b95b608fdfd85
Shay Telfer <shay.telfer@gaiaresources.com.au>
2021-10-26 09:53:53 +0800

    Add Sean's index_interrogator plugin
    (cherry picked from commit 908a807c07c0827a8bab36ceb54869699745b8e7)

--- plugins/bootstrap.sh
+++ plugins/bootstrap.sh
@@ -55,6 +55,10 @@ PLUGINS = [
   {
     :path => 'plugins/qsa_migration_adapter',
     :url => 'git@gitlab.gaiaresources.com.au:qsa/qsa_migration_adapter.git',
+  },
+  {
+    :path => 'plugins/index_interrogator',
+    :url => 'git@gitlab.gaiaresources.com.au:qsa/index_interrogator.git',
   }
 ]
 
