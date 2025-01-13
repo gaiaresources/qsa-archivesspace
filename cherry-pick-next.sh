6514a3ce4fd48826732f7f9d7048cd5abf4cc47e
Mark Triggs <mark@dishevelled.net>
2025-01-13 14:48:42 +1100

    Add scripts to support cherry pick

---
+++ cherry-pick-next.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REBASE_HEAD_COMMIT="f9f1712128832b04d5a340c0667c6d68e421b57f"
+
+function already_applied_commits {
+    git log --grep='(cherry picked from commit' ${REBASE_HEAD_COMMIT}.. | grep '(cherry picked from commit' | awk '{print $5}' | tr -d ")"
+}
+
+commit_id="`grep -v -f<(already_applied_commits) commits_to_apply | grep -v '^#' | head -1`"
+
+echo "Apply the following commit:"
+echo
+echo "  ${commit_id}"
+echo
+echo "========================================================================"
+git log -1 "$commit_id"
+echo "========================================================================"
+echo
+echo "To skip it:"
+echo
+echo "git cherry-pick --abort; cat commits_to_apply | sed 's/^${commit_id}/#${commit_id}/' > commits_to_apply.tmp && mv commits_to_apply.tmp commits_to_apply"
+echo
+
+git cherry-pick -x "$commit_id"
