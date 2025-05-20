archivesspace/archivesspace

BUGFIX: Don't blow up when fetching global records

backend/app/model/ASModel_crud.rb
@@ -363,7 +363,7 @@ module ASModel
           record_id = sequel_obj.id
 
           DB.after_commit do
-            hash = model.to_jsonmodel(record_id).to_hash(:trusted)
+            hash = model.any_repo.to_jsonmodel(record_id).to_hash(:trusted)
             RealtimeIndexing.record_update(hash, uri)
           end
         end

gaiaresources/qsa-archivesspace

(cherry picked from commit 32dd42bda09fd7cc65eb3260b91ea901c8599be8)

backend/app/model/ASModel_crud.rb
@@ -372,7 +372,7 @@ module ASModel
               # before trying to fire the update
               record = model.any_repo.filter(:id => record_id).first
               if record
-                hash = model.to_jsonmodel(record).to_hash(:trusted)
+                hash = model.any_repo.to_jsonmodel(record).to_hash(:trusted)
                 RealtimeIndexing.record_update(hash, uri)
               end
             end